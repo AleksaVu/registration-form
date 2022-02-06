361fde37563f47ea01a5c66ddc47795dc510ad2e
AleksaVu <vujosevic.aleksa404@gmail.com>
2022-02-06 21:46:47 +0100

    Change style of Success component

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Box from "@mui/material/Box";
+import logo from "../images/monkey-rocket.png";
+import Confetti from "react-confetti";
 
 export default class Success extends Component {
   render() {
@@ -11,7 +13,10 @@ export default class Success extends Component {
           alignItems: "center",
           minHeight: "100vh",
         }}>
+        <Confetti min-width="100%" min-height="100%" />
+        <img src={logo} className="image" width="200px" alt="Monkey" />
         <h1>Registration Successful</h1>
+        <img src={logo} className="image" width="200px" alt="Monkey" />
       </Box>
     );
   }
